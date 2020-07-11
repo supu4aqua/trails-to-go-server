8bf5f61bde8b83aa9f6cdac58e89018266461a6a
Supreet <supreet.kaur@live.com>
2020-07-10 20:44:41 -0400

    Modified config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://trail_admin@localhost/trails",
-  TEST_DATABASE_URLL:
+  TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://trail_admin@localhost/trails_test",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
